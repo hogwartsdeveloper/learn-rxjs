ecaaed5922e06723076f3e47bfb445b791f77b6f
Zhannur <jannuraidynuly@gmail.com>
2022-04-08 15:46:03 +0500

    operators

--- src/operators.js
+++ src/operators.js
@@ -1,12 +1,36 @@
-import { interval } from 'rxjs';
-import {map} from 'rxjs/operators'
+import { interval, fromEvent } from 'rxjs';
+import { map, filter, tap, take, takeLast, takeWhile, scan, reduce, switchMap } from 'rxjs/operators';
 
-const stream$ = interval(1000)
+fromEvent(document, 'click')
     .pipe(
-        
+        switchMap(() => {
+            return interval(1000)
+                    .pipe(
+                        tap(v => console.log('Tap: ', v)),
+                        take(5),
+                        reduce((acc, v) => acc + v, 0)
+                    )
+        })
     )
+    .subscribe({
+        next: v => console.log('Next', v),
+        complete: () => console.log('Complete')
+    })
 
-stream$.subscribe({
-    next: v => console.log('Next: ', v),
-    complete: () => console.log('Compleate')
-})
+// const stream$ = interval(1000)
+//     .pipe(
+//         tap(v=> console.log('Tap: ', v)),
+//         take(10),
+//         // map((v) => v * 2),
+//         // filter(v => v % 2 === 0),
+//         // take(10),
+//         // takeLast(5)
+//         // takeWhile(v => v < 7)
+//         // scan((acc, v) => acc + v, 0)
+//         reduce((acc, v) => acc + v, 0)
+//     )
+//
+// stream$.subscribe({
+//     next: v => console.log('Next', v),
+//     complete: () => console.log('Complete')
+// })
